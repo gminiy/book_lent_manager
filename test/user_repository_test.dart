fd9c3782ed0290a4eeec23594aea53467f4b799d
gminiy <gmini.y@gmail.com>
2023-12-19 21:00:01 +0900

    feat: userRepository getAllUsers 테스트

---
+++ test/user_repository_test.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:book_lent_manager/model/user.dart';
+import 'package:book_lent_manager/repository/user_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  final List<User> mockUsers = [User(
+    id: 0,
+    name: 'name',
+    gender: 1,
+    birthday: DateTime.now(),
+    address: 'address',
+    phoneNumber: '010-0000-0000',
+    createdAt: DateTime.now(),
+  ),
+    User(
+      id: 1,
+      name: 'name1',
+      gender: 2,
+      birthday: DateTime.now(),
+      address: 'address1',
+      phoneNumber: '010-1111-1111',
+      createdAt: DateTime.now(),
+    )
+  ];
+  List<String> mockUserList = mockUsers.map((mockUser) =>
+      jsonEncode(mockUser.toJson())).toList();
+
+  SharedPreferences.setMockInitialValues({'user': mockUserList});
+
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  UserRepository userRepository = UserRepository();
+
+  test('should return all users', () async {
+    List<User> allUsers = await userRepository.getAllUsers();
+    expect(allUsers, mockUsers);
+  });
+}
