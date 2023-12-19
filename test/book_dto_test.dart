59a5584003e46e06ade152825733f69bc84533c4
gminiy <gmini.y@gmail.com>
2023-12-19 23:12:29 +0900

    fix: bookDto 테스트 코드 수정

--- test/book_dto_test.dart
+++ test/book_dto_test.dart
@@ -8,12 +8,22 @@ void main() {
 
   test('bookDto with null toJson', () {
     BookDto bookDto = BookDto(id: 0);
-    print(bookDto.toJson());
+    Map<String, dynamic> expectBookDtoJson = {
+      'id': 0,
+      'name': null,
+      'isRented': null,
+      'rentedUserId': null,
+      'publishedDate': null,
+      'rentedAt': null,
+    };
+
+    expect(bookDto.toJson(), expectBookDtoJson);
   });
 
   test('bookDto with null fromJson', () {
-    BookDto bookDto = BookDto.fromJson({"id": 0});
+    BookDto bookDto = BookDto.fromJson({'id': 0});
     BookDto expectBookDto = BookDto(id: 0);
+
     expect(bookDto, expectBookDto);
   });
 }
