2a634c658cdeb340ffc0881f9586deed164ccb07
gminiy <gmini.y@gmail.com>
2023-12-19 16:23:57 +0900

    Feat: user dto id required 로 변경

---
+++ lib/mapper/user_mapper.dart
@@ -0,0 +1,16 @@
+import 'package:book_lent_manager/dto/user_dto.dart';
+import 'package:book_lent_manager/model/user.dart';
+
+extension on UserDto {
+  Future<User> toUser() async {
+    return User(
+      id: id,
+      name: name ?? '이름 없음',
+      gender: gender ?? 3,
+      birthday: birthday ?? DateTime(0),
+      address: address?? '주소 없음',
+      phoneNumber: phoneNumber ?? '연락처 없음',
+      createdAt: createdAt ?? DateTime(0),
+    );
+  }
+}
